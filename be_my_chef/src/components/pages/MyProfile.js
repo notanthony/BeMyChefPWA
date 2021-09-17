921832a2deab02b8f32959ffb6d3932db5c70bca
cookfisher <cookfisher@outlook.com>
2021-09-17 18:25:47 -0400

    update package.json

--- be_my_chef/src/components/pages/MyProfile.js
+++ be_my_chef/src/components/pages/MyProfile.js
@@ -1,28 +1,38 @@
 import React from 'react'
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
 import '../../styles/homepage.css'
 
 
-export default function MyProfile() {
+export default function MyProfile(props) {
     
     return (
+        
+        <View>
         <div className='homepage-flex'>
-            <h2 className='welcome-text'>J Y</h2>
+            
+            <h2 className='welcome-text'>{props.name}</h2>
             <img 
                 src='https://i.imgur.com/fe3m6vx_d.webp?maxwidth=760&fidelity=grand'
                 alt='user_pic'
                 className='main-img'
             >
             </img>
-            <h2 className='welcome-text'>J Y</h2>
+            
             <p>I love to cook in my free time</p>
-            <div>
+            <ScrollView style={{flex: 1}}>
+            
             <h2 className='profile-text'>J LIKED RECIPES</h2>
             <img 
-                src='https://i.imgur.com/fe3m6vx_d.webp?maxwidth=760&fidelity=grand'
+                src='https://spoonacular.com/recipeImages/716429-556x370.jpg'
                 alt='recipe_pic'
                 className='main-img'
             ></img>
-            </div>
+            
+            </ScrollView>
+          
         </div>
+        </View>
+        
+        
     )
 }
--- be_my_chef/src/components/pages/RecipesPage.js
+++ be_my_chef/src/components/pages/RecipesPage.js
@@ -1,10 +1,426 @@
 import React from 'react'
+import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
 
 export default function RecipesPage() {
+    var recipe;
+    // not yet tested
+    // fetch('https://api.spoonacular.com/recipes/' + ? + '/information')
+    //    .then(res => recipe = res)
+    // Just simulating the thing we may get from the API call
+    recipe = {
+        "title": "Pasta with Garlic, Scallions, Cauliflower & Breadcrumbs",
+        "image": "https://spoonacular.com/recipeImages/716429-556x370.jpg",
+        "readyInMinutes": 45,
+        "healthScore": 19.0,
+        "pricePerServing": 163.15,
+        "cuisines": [],
+        "dairyFree": false,
+        "diets": [],
+        "gaps": "no",
+        "glutenFree": false,
+        "instructions": "Put the garlic in a pan and then add the onion. Add some salt and oregano.",
+        "ketogenic": false,
+        "lowFodmap": false,
+        "occasions": [],
+        "sustainable": false,
+        "vegan": false,
+        "vegetarian": false,
+        "veryHealthy": false,
+        "veryPopular": false,
+        "whole30": false,
+        "extendedIngredients": [
+            {
+                "aisle": "Milk, Eggs, Other Dairy",
+                "amount": 1.0,
+                "consitency": "solid",
+                "id": 1001,
+                "image": "butter-sliced.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 1.0,
+                        "unitLong": "Tbsp",
+                        "unitShort": "Tbsp"
+                    },
+                    "us": {
+                        "amount": 1.0,
+                        "unitLong": "Tbsp",
+                        "unitShort": "Tbsp"
+                    }
+                },
+                "meta": [],
+                "name": "butter",
+                "original": "1 tbsp butter",
+                "originalName": "butter",
+                "unit": "tbsp"
+            },
+            {
+                "aisle": "Produce",
+                "amount": 2.0,
+                "consitency": "solid",
+                "id": 10011135,
+                "image": "cauliflower.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 473.176,
+                        "unitLong": "milliliters",
+                        "unitShort": "ml"
+                    },
+                    "us": {
+                        "amount": 2.0,
+                        "unitLong": "cups",
+                        "unitShort": "cups"
+                    }
+                },
+                "meta": [
+                    "frozen",
+                    "thawed",
+                    "cut into bite-sized pieces"
+                ],
+                "name": "cauliflower florets",
+                "original": "about 2 cups frozen cauliflower florets, thawed, cut into bite-sized pieces",
+                "originalName": "about frozen cauliflower florets, thawed, cut into bite-sized pieces",
+                "unit": "cups"
+            },
+            {
+                "aisle": "Cheese",
+                "amount": 2.0,
+                "consitency": "solid",
+                "id": 1041009,
+                "image": "cheddar-cheese.png",
+                "measures": {
+                    "metric": {
+                        "amount": 2.0,
+                        "unitLong": "Tbsps",
+                        "unitShort": "Tbsps"
+                    },
+                    "us": {
+                        "amount": 2.0,
+                        "unitLong": "Tbsps",
+                        "unitShort": "Tbsps"
+                    }
+                },
+                "meta": [
+                    "grated",
+                    "(I used romano)"
+                ],
+                "name": "cheese",
+                "original": "2 tbsp grated cheese (I used romano)",
+                "originalName": "grated cheese (I used romano)",
+                "unit": "tbsp"
+            },
+            {
+                "aisle": "Oil, Vinegar, Salad Dressing",
+                "amount": 1.0,
+                "consitency": "liquid",
+                "id": 1034053,
+                "image": "olive-oil.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 1.0,
+                        "unitLong": "Tbsp",
+                        "unitShort": "Tbsp"
+                    },
+                    "us": {
+                        "amount": 1.0,
+                        "unitLong": "Tbsp",
+                        "unitShort": "Tbsp"
+                    }
+                },
+                "meta": [],
+                "name": "extra virgin olive oil",
+                "original": "1-2 tbsp extra virgin olive oil",
+                "originalName": "extra virgin olive oil",
+                "unit": "tbsp"
+            },
+            {
+                "aisle": "Produce",
+                "amount": 5.0,
+                "consitency": "solid",
+                "id": 11215,
+                "image": "garlic.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 5.0,
+                        "unitLong": "cloves",
+                        "unitShort": "cloves"
+                    },
+                    "us": {
+                        "amount": 5.0,
+                        "unitLong": "cloves",
+                        "unitShort": "cloves"
+                    }
+                },
+                "meta": [],
+                "name": "garlic",
+                "original": "5-6 cloves garlic",
+                "originalName": "garlic",
+                "unit": "cloves"
+            },
+            {
+                "aisle": "Pasta and Rice",
+                "amount": 6.0,
+                "consitency": "solid",
+                "id": 20420,
+                "image": "fusilli.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 170.097,
+                        "unitLong": "grams",
+                        "unitShort": "g"
+                    },
+                    "us": {
+                        "amount": 6.0,
+                        "unitLong": "ounces",
+                        "unitShort": "oz"
+                    }
+                },
+                "meta": [
+                    "(I used linguine)"
+                ],
+                "name": "pasta",
+                "original": "6-8 ounces pasta (I used linguine)",
+                "originalName": "pasta (I used linguine)",
+                "unit": "ounces"
+            },
+            {
+                "aisle": "Spices and Seasonings",
+                "amount": 2.0,
+                "consitency": "solid",
+                "id": 1032009,
+                "image": "red-pepper-flakes.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 2.0,
+                        "unitLong": "pinches",
+                        "unitShort": "pinches"
+                    },
+                    "us": {
+                        "amount": 2.0,
+                        "unitLong": "pinches",
+                        "unitShort": "pinches"
+                    }
+                },
+                "meta": [
+                    "red"
+                ],
+                "name": "red pepper flakes",
+                "original": "couple of pinches red pepper flakes, optional",
+                "originalName": "couple of red pepper flakes, optional",
+                "unit": "pinches"
+            },
+            {
+                "aisle": "Spices and Seasonings",
+                "amount": 2.0,
+                "consitency": "solid",
+                "id": 1102047,
+                "image": "salt-and-pepper.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 2.0,
+                        "unitLong": "servings",
+                        "unitShort": "servings"
+                    },
+                    "us": {
+                        "amount": 2.0,
+                        "unitLong": "servings",
+                        "unitShort": "servings"
+                    }
+                },
+                "meta": [
+                    "to taste"
+                ],
+                "name": "salt and pepper",
+                "original": "salt and pepper, to taste",
+                "originalName": "salt and pepper, to taste",
+                "unit": "servings"
+            },
+            {
+                "aisle": "Produce",
+                "amount": 3.0,
+                "consitency": "solid",
+                "id": 11291,
+                "image": "spring-onions.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 3.0,
+                        "unitLong": "",
+                        "unitShort": ""
+                    },
+                    "us": {
+                        "amount": 3.0,
+                        "unitLong": "",
+                        "unitShort": ""
+                    }
+                },
+                "meta": [
+                    "white",
+                    "green",
+                    "separated",
+                    "chopped"
+                ],
+                "name": "scallions",
+                "original": "3 scallions, chopped, white and green parts separated",
+                "originalName": "scallions, chopped, white and green parts separated",
+                "unit": ""
+            },
+            {
+                "aisle": "Alcoholic Beverages",
+                "amount": 2.0,
+                "consitency": "liquid",
+                "id": 14106,
+                "image": "white-wine.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 2.0,
+                        "unitLong": "Tbsps",
+                        "unitShort": "Tbsps"
+                    },
+                    "us": {
+                        "amount": 2.0,
+                        "unitLong": "Tbsps",
+                        "unitShort": "Tbsps"
+                    }
+                },
+                "meta": [
+                    "white"
+                ],
+                "name": "white wine",
+                "original": "2-3 tbsp white wine",
+                "originalName": "white wine",
+                "unit": "tbsp"
+            },
+            {
+                "aisle": "Pasta and Rice",
+                "amount": 0.25,
+                "consitency": "solid",
+                "id": 99025,
+                "image": "breadcrumbs.jpg",
+                "measures": {
+                    "metric": {
+                        "amount": 59.147,
+                        "unitLong": "milliliters",
+                        "unitShort": "ml"
+                    },
+                    "us": {
+                        "amount": 0.25,
+                        "unitLong": "cups",
+                        "unitShort": "cups"
+                    }
+                },
+                "meta": [
+                    "whole wheat",
+                    "(I used panko)"
+                ],
+                "name": "whole wheat bread crumbs",
+                "original": "1/4 cup whole wheat bread crumbs (I used panko)",
+                "originalName": "whole wheat bread crumbs (I used panko)",
+                "unit": "cup"
+            }
+        ],
+    }
+    var ingredientsList = [];
+    for(var i of recipe.extendedIngredients) {
+        ingredientsList.push(i.original);
+    }
+    var instructions;
+    // not yet tested
+    // fetch('https://api.spoonacular.com/recipes/analyzeInstructions?apiKey=e17b28ec56034c8e82ed9212db2c5e55&instructions=' + recipe.instructions, {method: 'POST'})
+    //    .then(res => instructions = res.parsedInstructions[0].steps)
+    // Just simulating the thing we may get from the API call
+    instructions = {
+        "parsedInstructions": [
+            {
+                "name": "",
+                "steps": [
+                    {
+                        "number": 1,
+                        "step": "Put the garlic in a pan and then add the onion.",
+                        "ingredients": [
+                            {
+                                "id": 11215,
+                                "name": "garlic",
+                                "localizedName": "garlic",
+                                "image": "garlic.png"
+                            },
+                            {
+                                "id": 11282,
+                                "name": "onion",
+                                "localizedName": "onion",
+                                "image": "brown-onion.png"
+                            }
+                        ],
+                        "equipment": [
+                            {
+                                "id": 404645,
+                                "name": "frying pan",
+                                "localizedName": "frying pan",
+                                "image": "pan.png"
+                            }
+                        ]
+                    },
+                    {
+                        "number": 2,
+                        "step": "Add some salt and oregano.",
+                        "ingredients": [
+                            {
+                                "id": 2027,
+                                "name": "oregano",
+                                "localizedName": "oregano",
+                                "image": "oregano.jpg"
+                            },
+                            {
+                                "id": 2047,
+                                "name": "salt",
+                                "localizedName": "salt",
+                                "image": "salt.jpg"
+                            }
+                        ],
+                        "equipment": []
+                    }
+                ]
+            }
+        ],
+        "ingredients": [
+            {
+                "id": 2027,
+                "name": "oregano"
+            },
+            {
+                "id": 11215,
+                "name": "garlic"
+            },
+            {
+                "id": 11282,
+                "name": "onion"
+            },
+            {
+                "id": 2047,
+                "name": "salt"
+            }
+        ],
+        "equipment": [
+            {
+                "id": 404645,
+                "name": "frying pan"
+            }
+        ]
+    }.parsedInstructions[0].steps
     return (
         <div>
             From ./components/pages/RecipesPage
-            
+            <h2>{recipe.title}</h2>
+            <ScrollView>
+            <img 
+                src={recipe.image}
+                alt='icon'
+                className='main-img'
+            />
+            <h3>You will need:</h3>
+            <ul>{ingredientsList.map(item => {return <li key={item}>{item}</li>;})}</ul>
+            <h3>Instructions</h3>
+            <ol>{instructions.map(item => {return <li key={item.number}>{item.step}</li>})}</ol>
+            </ScrollView>
         </div>
+        
     )
 }
