4e5d1a00bbd9d733aa162e803b763ef0e283cfef
unknown <anthony.andy.zhao@gmail.com>
2021-09-17 14:57:13 -0400

    SnapChat share component / surveypage just has sample implementation

--- be_my_chef/src/components/pages/SurveyPage.js
+++ be_my_chef/src/components/pages/SurveyPage.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import '../../styles/survey.css'
+import {SnapShareButton} from '../global/Share'
 
 export default function SurveyPage() {
     return (
         <div className='survey-main'>
             <div className='survey-header-card'>
-                
+                <SnapShareButton url = "https://notanthony.github.io/LTDC"/>
             </div>
         </div>
     )
