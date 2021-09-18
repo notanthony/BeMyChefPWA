2f798a70f2cc8230f2d0d99608d51ac1c4134f72
cookfisher <cookfisher@outlook.com>
2021-09-17 22:22:16 -0400

    minor

--- be_my_chef/src/components/pages/SingleRecipe.js
+++ be_my_chef/src/components/pages/SingleRecipe.js
@@ -1,12 +1,9 @@
 import React from 'react'
-import ShareButton from '../global/ShareButton'
-
 
 export default function SingleRecipe() {
     return (
         <div>
             From ./components/pages/SingleRecipe
-            <ShareButton />
             
         </div>
     )
