2a535018cc1654bd5e76bf01eda54901c767a5b2
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2021-09-17 20:05:39 -0400

    packagelock changes + profile changes

--- be_my_chef/src/components/pages/MyProfile.js
+++ be_my_chef/src/components/pages/MyProfile.js
@@ -1,48 +1,28 @@
 import React from 'react'
-<<<<<<< Updated upstream
-import { StyleSheet, Text, SafeAreaView, ScrollView, StatusBar, View } from 'react-native';
-import '../../styles/homepage.css'
-
-=======
 import '../../styles/default.css'
 import MainHeader from '../global/MainHeader'
->>>>>>> Stashed changes
+import RecipesPage from './RecipesPage'
 
 export default function MyProfile(props) {
     
     return (
-<<<<<<< Updated upstream
-        
-        <View>
-        <div className='homepage-flex'>
-            
-            <h2 className='welcome-text'>{props.name}</h2>
-            <img 
-                src='https://i.imgur.com/fe3m6vx_d.webp?maxwidth=760&fidelity=grand'
-                alt='user_pic'
-                className='main-img'
-            >
-            </img>
-            
-            <p>I love to cook in my free time</p>
-            <ScrollView style={{flex: 1}}>
-            
-            <h2 className='profile-text'>J LIKED RECIPES</h2>
-            <img 
-                src='https://spoonacular.com/recipeImages/716429-556x370.jpg'
-                alt='recipe_pic'
-                className='main-img'
-            ></img>
-            
-            </ScrollView>
-          
-=======
         <div className='main-flex'>
             <MainHeader />
-
->>>>>>> Stashed changes
+            <div className='scrollable-div'>
+                <h1>Profile Name</h1>
+                <img className='profile-img' alt='profile img' src='https://i.imgur.com/vzIo9qz.jpeg'>
+                </img>
+                <h3 className='profile-desc'>
+                    I love to cook in my free time.<br/> My favorite foods are broccoli and cherries.
+                </h3>
+                <div className='favorites-div'>
+                    <h1>Profile's Liked Recipes</h1>
+                    <div className='recipes-display'>
+                        <RecipesPage />
+                    </div>
+                </div>
+            </div>
         </div>
-        </View>
         
         
     )
