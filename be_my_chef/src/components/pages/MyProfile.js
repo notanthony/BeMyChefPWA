1e2cd1a6daeace137ac772c98b13e9b72a8527bc
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2021-09-18 17:39:53 -0400

    more css fixes

--- be_my_chef/src/components/pages/MyProfile.js
+++ be_my_chef/src/components/pages/MyProfile.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../../styles/default.css'
 import '../../styles/profile.css'
 import MainHeader from '../global/MainHeader'
 import RecipesPage from './RecipesPage'
@@ -11,25 +10,25 @@ export default function MyProfile(props) {
     return (
         <div>
             <MainHeader />
-        <div className='main-flex'>
-            <div className='scrollable-div'>
-                <h1>Profile Name</h1>
-                <img className='profile-img' alt='profile img' src='https://i.imgur.com/vzIo9qz.jpeg'>
-                </img>
-                <h3 className='profile-desc'>
-                    I love to cook in my free time.<br/> My favorite foods are broccoli and cherries.
-                </h3>
-                <Link className='survey-btn' to='/survey'>
-                    Retake Survey
-                 </Link>
-                <div className='favorites-div'>
-                    <h1>Profile's Liked Recipes</h1>
-                    <div className='recipes-display'>
-                        <RecipesPage />
+            <div className='main-flex'>
+                <div className='scrollable-div'>
+                    <h1>Profile Name</h1>
+                    <img className='profile-img' alt='profile img' src='https://i.imgur.com/vzIo9qz.jpeg'>
+                    </img>
+                    <h3 className='profile-desc'>
+                        I love to cook in my free time.<br/> My favorite foods are broccoli and cherries.
+                    </h3>
+                    <Link className='survey-btn' to='/survey'>
+                        Retake Survey
+                    </Link>
+                    <div className='favorites-div'>
+                        <h1>Profile's Liked Recipes</h1>
+                        <div className='recipes-display'>
+                            <RecipesPage />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
         </div>
         
         
