f6a251e2f10f036db3739d3e841a3159809d648c
Eric Zhang <46907515+ericzhang11101@users.noreply.github.com>
2021-09-18 20:55:34 -0400

    work on share button + guilds

--- be_my_chef/src/api.js
+++ be_my_chef/src/api.js
@@ -1,6 +1,3 @@
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
-const $ = require( "jquery" )( window );
 
 function arrayToAPIString(arr) {
     if (Array.isArray(arr)) {
@@ -9,36 +6,36 @@ function arrayToAPIString(arr) {
     return arr
   }
 
-function complexSearch(mealtype = "none", intolerances = "none", cuisine= "none", equipment="none", difficulty = 1){
-    $.ajax({
-        url : "https://api.spoonacular.com/recipes/search?apiKey=5d197e15a4b34ee7934de1fef766b366&mealtype=" + arrayToAPIString(mealtype) +
-        "&intolerances=" + arrayToAPIString(intolerances) + "&cuisine=" + arrayToAPIString(cuisine) + "&equipment=" + arrayToAPIString(equipment) + "&maxReadyTime=60", 
-        success: function(res){
-            var dict = res.results;
-            //sorting in terms of the ready time, could be made more efficient if time permits
-            for( let i  =0; i < res.number ; i++){
-                var current = dict[i];
-                let j = i-1;
-                while((j > -1) && (parseInt(current.readyInMinutes) < parseInt(dict[j].readyInMinutes))){
-                    dict[j+1] = dict[j];
-                    j--;
-                }
-                dict[j+1] = current;
-            }
-            if(difficulty == 1){
-                return dict[0] //easiest
-            }
-            else if(difficulty ==2){
-                return dict[parseInt(res.number) /2] //midpack
-            }
-            else{
-                return dict[parseInt(res.number) -1] //hardest
-            }
+// function complexSearch(mealtype = "none", intolerances = "none", cuisine= "none", equipment="none", difficulty = 1){
+//     $.ajax({
+//         url : "https://api.spoonacular.com/recipes/search?apiKey=5d197e15a4b34ee7934de1fef766b366&mealtype=" + arrayToAPIString(mealtype) +
+//         "&intolerances=" + arrayToAPIString(intolerances) + "&cuisine=" + arrayToAPIString(cuisine) + "&equipment=" + arrayToAPIString(equipment) + "&maxReadyTime=60", 
+//         success: function(res){
+//             var dict = res.results;
+//             //sorting in terms of the ready time, could be made more efficient if time permits
+//             for( let i  =0; i < res.number ; i++){
+//                 var current = dict[i];
+//                 let j = i-1;
+//                 while((j > -1) && (parseInt(current.readyInMinutes) < parseInt(dict[j].readyInMinutes))){
+//                     dict[j+1] = dict[j];
+//                     j--;
+//                 }
+//                 dict[j+1] = current;
+//             }
+//             if(difficulty == 1){
+//                 return dict[0] //easiest
+//             }
+//             else if(difficulty ==2){
+//                 return dict[parseInt(res.number) /2] //midpack
+//             }
+//             else{
+//                 return dict[parseInt(res.number) -1] //hardest
+//             }
             
-        }
-    })
-    return null 
-}
+//         }
+//     })
+//     return null 
+// }
 
 // ingredients search (minimum missing elements)
 // ingredients - array of ingredients
@@ -66,4 +63,4 @@ function complexSearch(mealtype = "none", intolerances = "none", cuisine= "none"
     return ret;
 }
 
-export {ingredientsSearch, complexSearch}
+export {ingredientsSearch}
